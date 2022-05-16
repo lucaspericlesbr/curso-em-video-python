c212ad2defec3c3bb65bd988be63ac827b873a9a
lucaspericlesbr <lucaspericlesbr@gmail.com>
2022-05-16 00:05:31 -0300

    Basic exercice.

---
+++ fundamentals/Ex002.py
@@ -0,0 +1,6 @@
+'''
+Create a pyhton script that reads a person's name and displays a welcome message according to the value entered.
+'''
+
+name = input('What is your name? ')
+print('Hello {}! Nice to meet you!'.format(name))
