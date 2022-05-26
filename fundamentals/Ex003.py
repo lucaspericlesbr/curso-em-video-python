8901489af4b6ea9c04be286711fe27357f268022
lucaspericlesbr <lucaspericlesbr@gmail.com>
2022-05-25 21:44:24 -0300

    Learning about primitive types, data output and variables.

---
+++ fundamentals/Ex003.py
@@ -0,0 +1,9 @@
+'''
+Create a program that reads two numbers and shows the sum between them.
+'''
+
+n1 = int(input('Type a value: '))
+n2 = int(input('Type another value: '))
+s = n1 + n2
+
+print('The sum between {} and {} is equal to {}.'.format(n1, n2, s))
