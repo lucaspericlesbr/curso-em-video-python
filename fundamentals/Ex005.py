c48184936c1f5528c8e6c72be7ce41dfa26618ab
lucaspericlesbr <lucaspericlesbr@gmail.com>
2022-05-29 23:36:37 -0300

    Learning about arithmetic operators.

---
+++ fundamentals/Ex005.py
@@ -0,0 +1,7 @@
+'''
+Make a program that reads an integer and shows your successor and predecessor on the screen.
+'''
+
+n = int(input('Enter an integer: '))
+print('The value entered was {}, your predecessor is {} and your sucessor is {}.'.format(n, (n-1), (n+1)))
+
