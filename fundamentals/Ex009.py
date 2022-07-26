3b1a0e76171a1da205e60c6879a00b56844eb037
lucaspericlesbr <lucaspericlesbr@gmail.com>
2022-07-26 17:03:03 -0300

    Learning about arithmetic operators.

---
+++ fundamentals/Ex009.py
@@ -0,0 +1,18 @@
+'''
+Make a program that reads any whole number and shows your multiplication table on the screen.
+'''
+
+num = int(input('Enter a value: '))
+
+print(20 * '=')
+print('{} X {:2} = {}'.format(num, 1, (num*1)))
+print('{} X {:2} = {}'.format(num, 2, (num*2)))
+print('{} X {:2} = {}'.format(num, 3, (num*3)))
+print('{} X {:2} = {}'.format(num, 4, (num*4)))
+print('{} X {:2} = {}'.format(num, 5, (num*5)))
+print('{} X {:2} = {}'.format(num, 6, (num*6)))
+print('{} X {:2} = {}'.format(num, 7, (num*7)))
+print('{} X {:2} = {}'.format(num, 8, (num*8)))
+print('{} X {:2} = {}'.format(num, 9, (num*9)))
+print('{} X {:2} = {}'.format(num, 10, (num*10)))
+print(20 * '=')
